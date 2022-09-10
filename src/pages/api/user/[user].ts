fd5afe80f713bda71a530204577a3e7e7fd3caa5
pdcolandrea <colandreapaul@gmail.com>
2022-09-10 17:41:20 -0400

    feat: add cache headers

--- src/pages/api/user/[user].ts
+++ src/pages/api/user/[user].ts
@@ -7,7 +7,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(res.status(200).send(`hello ${user}`));
+      resolve(
+        res
+          .setHeader('Cache-Control', 's-maxage=20')
+          .setHeader('Cache-Control', 'max-age=99')
+          .status(200)
+          .send(`hello ${user}`)
+      );
     }, 5000);
   });
 }
